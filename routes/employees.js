5da5c93836e35309f7aaa68899f72de064b11380
Raul <Raul.Camargo89@myHunter.CUNY.edu>
2022-12-07 19:03:49 -0500

    Added task route for get single and edit

--- routes/employees.js
+++ routes/employees.js
@@ -5,10 +5,10 @@ const {Task, Employee} = require('../database/models');
 // async handler
 const ash = require('express-async-handler');
 
-// Get All tasks
+// Get All employees
 router.get('/', ash(async(req, res) =>
 {
-    let employees = await Employee.findAll({include: [Task]});
+    let employees = await Employee.findAll();
     res.status(200).json(employees);
 }));
 
--- routes/tasks.js
+++ routes/tasks.js
@@ -8,8 +8,24 @@ const ash = require('express-async-handler');
 // Get All tasks
 router.get('/', ash(async(req, res) =>
 {
-    let tasks = await Task.findAll({include: [Employee]});
+    let tasks = await Task.findAll();
     res.status(200).json(tasks);
+}));
+
+// Get  single task by id & assigned employee
+router.get('/:id', ash(async(req, res) =>
+{
+    let task = await Task.findByPk(req.params.id, {include: [Employee]});
+    res.status(200).json(task);
+}));
+
+// Edit task
+router.put('/:id', ash(async(req, res) =>
+{
+    await Task.update(req.body, {where: {id: req.params.id}});
+
+    let task = await Task.findByPk(req.params.id);
+    res.status(201).json(task);
 }));
 
 module.exports = router;
